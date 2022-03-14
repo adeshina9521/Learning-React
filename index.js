68b955d5306d9f13546a03ebd76fb58bfcf8a4bb
adeshina9521 <abdullateef.adegoke@gmail.com>
2022-03-14 16:50:55 +0100

    learning JSX'

--- index.js
+++ index.js
@@ -6,7 +6,16 @@
 // ReactDOM.render(<div><MainContent/></div>, document.getElementById("root"))
 
 
-const h1 = document.createElement("h1");
-h1.textContent = "this is an imparative way to program";
-h1.className = "header";
-document.getElementById("root").append(h1)
+// const h1 = document.createElement("h1");
+// h1.textContent = "this is an imparative way to program";
+// h1.className = "header";
+// document.getElementById("root").append(h1)
+
+const page = (<div>
+                <h1>This is JSX</h1>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim.</p>
+            </div>
+                )                
+ReactDOM.render( page
+                ,
+                 document.getElementById("root"))
